eec95c3b6221f1f236529b496f7adaf283f1f373
kazuuuuma <kazuma.hayakawa.1024@gmail.com>
2020-10-14 21:06:39 +0900

    既読機能のサーバーサイド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,4 +7,15 @@ class PostsController < ApplicationController
   def create
     Post.create(content: params[:content])
   end
-end
+
+  def checked
+    post = Post.find(papams[:id])
+    if post.checked
+      post.update(checked: false)
+    else
+      post.update(checked:ture)
+    end
+
+    item Post.find(params[:id])
+    render jdon: { post: item }
+  end
